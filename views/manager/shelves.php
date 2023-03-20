449ddba9dcd59c48037b3cc94a349a498c754a00
NamesUnoriginal <alexjn0504@gmail.com>
2023-03-20 14:33:06 +0000

    Added skeleton for shelves menu

--- views/manager/shelves.php
+++ views/manager/shelves.php
@@ -2,11 +2,16 @@
     include_once("/Applications/MAMP/htdocs/CoolKaftanGroup17/includes/navbar.php");
 ?>
 
-
-
 <body>
     <div class="d-flex justify-content-center align-items-center">
-        
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <h1 class="fw-bold text-center my-2">Long Stock</h1>
+            <a href="topShelf.php" class="btn btn-success my-2">Top Shelf</a>
+            <a href="p1.php" class="btn btn-success my-2">P1</a>
+            <a href="p2.php" class="btn btn-success my-2">P2</a>
+            <a href="p3.php" class="btn btn-success my-2">P3</a>
+            <a href="p4.php" class="btn btn-success my-2">P4</a>
+        </div>
     </div>
     
 </body>
