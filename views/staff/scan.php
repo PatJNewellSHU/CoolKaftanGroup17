2aecb56c940e0abde4d4deb7fa35ac7ff42a6b54
NamesUnoriginal <alexjn0504@gmail.com>
2023-03-22 16:02:25 +0000

    ..

--- views/staff/scan.php
+++ views/staff/scan.php
@@ -45,8 +45,10 @@
             </div>
          </div>
       </main>
-      <script src = "../../js/reader.js"></script>
-      <script>
+      <?php
+         include("../../js/reader.php")
+      ?>
+      <!-- <script>
          // Copyright fix ;)
          var scan = document.querySelectorAll("a[href='https://scanapp.org']");
          scan[0].innerHTML = "Group 17";
@@ -54,7 +56,7 @@
          var report = document.querySelectorAll("a[href='https://github.com/mebjas/html5-qrcode/issues']");
          report[0].innerHTML = "Don't report issues."
          report[0].href = "https://www.youtube.com/watch?v=oHg5SJYRHA0";
-      </script>
+      </script> -->
    </div>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
