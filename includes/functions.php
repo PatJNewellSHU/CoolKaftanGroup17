25820f7e95e15f73098faf7f40c4139a4f50fdfa
NamesUnoriginal <alexjn0504@gmail.com>
2023-03-22 15:46:00 +0000

    added getBox functions

--- includes/functions.php
+++ includes/functions.php
@@ -48,3 +48,20 @@ function getUsers($conn, $username) {
 }
 
 //Item functions
+
+function GetBoxShelf($conn, $shelfID) {
+
+    $stmt = $conn -> prepare("SELECT * FROM box WHERE shelf_id = ?");
+    $stmt -> bind_param("s", $shelfID);
+    $stmt -> execute();
+
+    $result = $stmt -> get_result();
+
+    $row = $result -> fetch_assoc();
+
+    $stmt -> close();
+    $conn -> close();
+
+    return $row;
+
+}
