229fb4c5729b7a71987dd1246454573613f29e25
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2023-03-22 08:01:33 -0700

    fix - pat

--- js/reader.js
+++ js/reader.js
@@ -1,4 +1,4 @@
-window.result = null;
+
  const scanner = new Html5QrcodeScanner('reader', {
     qrbox: {
     width: 250,
@@ -10,7 +10,7 @@ window.result = null;
     function success(result) {
     document.getElementById('result').innerHTML = `
     <h2>Success!</h2>
-    <p><a href="${result}">${result}</a></p>
+    <p>${result}</p>
     `;
     scanner.clear();
     document.getElementById('reader').remove();
