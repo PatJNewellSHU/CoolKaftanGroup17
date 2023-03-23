f7d004ffc0671ae712e3ba85ef3dfd4694c6a8e9
NamesUnoriginal <alexjn0504@gmail.com>
2023-03-23 16:22:14 +0000

    slight function changes

--- includes/functions.php
+++ includes/functions.php
@@ -51,10 +51,10 @@ function MoveToBuffer($conn, $boxID) {
 
     $sql = "INSERT INTO buffer (product_id, number_of_items) VALUES (?, ?)";
 
-    $stmt = $conn ->prepare($sql);
-    $stmt -> bind_param("ss", $row["product_id"], $row["units"]);
+    $stmt2 = $conn ->prepare($sql);
+    $stmt2 -> bind_param("ss", $row["product_id"], $row["units"]);
 
-    if ($conn->query($stmt) === TRUE) {
+    if ($conn->query($stmt2) === TRUE) {
     echo "New record created successfully";
     } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
