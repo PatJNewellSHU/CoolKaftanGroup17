99d863bdf1ee9a444c4e5b65fc5ab072f4f89b66
NamesUnoriginal <alexjn0504@gmail.com>
2023-03-27 01:01:53 +0100

    help

---
+++ App/Helpers/authenticationHelper.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Helpers;
+
+class authenticationHelper {
+
+    public static function isAuth($type)
+    {
+        $enabled = true;
+
+        if($enabled == true)
+        {
+            session_start();
+            if(!isset($_SESSION['userId'])) {
+                header("location: /400");
+            }
+
+            if($_SESSION['userType'] != $type)
+            {
+                header("location: /400");
+            }
+        }
+    }
+
+    public static function isGuest()
+    {
+        $enabled = true;
+        
+        if($enabled == true)
+        {
+            
+            if(isset($_SESSION['userId'])) {
+                if($_SESSION['userType'] == 'staff')
+                {
+                    header("location: /staff");
+                }
+
+                if($_SESSION['userType'] == 'manager')
+                {
+                    header("location: /manager");
+                }
+            }
+        }
+    }
+
+}
---
+++ autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+// This file pre-loads all the other files so code is cleaner.
+// Doesn't impact performance as this is a tiny project.
+
+// Controllers
+require_once(__DIR__."/App/Controllers/loginController.php");
+require_once(__DIR__."/App/Controllers/managerController.php");
+require_once(__DIR__."/App/Controllers/staffController.php");
+
+// Helpers
+require_once(__DIR__ . "/App/Helpers/dbHelper.php");
+require_once(__DIR__ . "/App/Helpers/authenticationHelper.php");
+require_once(__DIR__ . "/App/Helpers/mailHelper.php");
+
+// Models
+require_once(__DIR__ . "/App/Models/productModel.php");
