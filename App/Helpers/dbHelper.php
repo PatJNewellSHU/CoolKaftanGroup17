fa3f65f2d0e106fe6b3bad29289c2f85d815a46c
NamesUnoriginal <alexjn0504@gmail.com>
2023-03-29 19:16:04 +0100

    Re-wrote delete function in dbhelper

--- App/Helpers/dbHelper.php
+++ App/Helpers/dbHelper.php
@@ -20,19 +20,7 @@ class dbHelper {
     }
 
 
-    public function get($table, $columns="*", $query=null, $associate=false)
-    {
-        $prepare = "SELECT " . $columns . " FROM " . $table . " ";
-
-        if($query != null)
-        {
-            $prepare = $prepare . $query;
-        }
-
-        return $this->query($prepare, $associate);
-    }
-
-    public function query($query, $associate)
+    public function query($query, $associate, $params, $columns, $table, $values)
     {
         $statement = $this->connection->prepare($query);
 
@@ -51,8 +39,10 @@ class dbHelper {
         if($associate == true)
         {
             $results = $results->fetch_assoc();
-        } else {
-            $results = $results->fetch_all();
+        } 
+        else
+        {
+            $results = $results->fetch_all(MYSQLI_ASSOC);
         }
 
         $this->connection->close();
@@ -60,10 +50,11 @@ class dbHelper {
         return $results;
     }
 
-    public function add($table, $columns=[], $values=[])
+    public function create($table, $columns=[], $values=[])
     {
 
-        $prepare = "INSERT INTO " . $table . "(".implode(', ', $columns).") VALUES (" .implode(', ', $values).");";
+        $prepare = "INSERT INTO " . $table . "('".implode("','", $columns)."') VALUES ('" .implode("','", $values).");";
+        //^ Cant test til database is finished but I think this should prevent having to add quotations around each variable beforehand? 
         
         $statement = $this->connection->prepare($prepare);
         $statement->execute();
@@ -71,11 +62,48 @@ class dbHelper {
         return true;
     }
 
+    public function read($table, $columns="*", $query=null, $associate=false)
+    {
+        $prepare = "SELECT ? FROM ?"; 
+        //^ removed quotes at the end as I dont think they are needed?
+
+        $params = "ss";
+
+        if($query != null)
+        {
+            $prepare = $prepare . $query;
+        }
+
+        return $this->query($prepare, $associate);
+    }
+
+    public function update($table, $columns = [], $values = []) {
+
+    }
+
     public function delete($table, $columns = [], $values = [])
     {
 
-        $prepare = "DELETE FROM " . $table . " WHERE '$columns[0]' = '$values[0]' AND '$columns[1]' = '$values[1]';";
+        $prepare = "DELETE FROM ? WHERE";
+        $params = array();
+        for($i = 0; $i < count($columns); $i++) //Should go through as many columns are in the array columns and add to the sql statement
+        {
+            if($i = 0) //This seems like a bad way to check if were on the first run of the for loop but idk how else to do it in php
+            {
+                $prepare .= " ? = ?";
+            }
+            else
+            {
+                $prepare .= " AND ? = ?";
+            }
+            
+            $params[] = $columns[$i];
+            $params[] = $values[$i];
+        }  
+
         $statement = $this->connection->prepare($prepare);
+        $types = str_repeat("s", count($columns)); //creates the variable types - kind of temp as it wont work with numbers but idk how the database looks yet
+        $statement -> bind_param($types, $table, ...$params); //SHOULD go through and attatch a variable to each ? in statement. 
         $statement->execute();
 
         return true;
@@ -84,4 +112,6 @@ class dbHelper {
     Tried just taking each string out of the array rather than imploding as I wanted to use "AND" in a delete statement, but not actually removing from the database.
     push#2
     */
+
+    
 }
