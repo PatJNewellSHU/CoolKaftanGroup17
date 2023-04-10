67a3479a6347b5396d831799b8dcaf839f7f579d
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2023-04-10 08:07:54 -0700

    help pls god

--- js/reader.js
+++ js/reader.js
@@ -7,13 +7,13 @@
     fps: 20,
     });
     scanner.render(success, error);
-    function success(result) {
+    function success(decodedText, decodedResult) {
 
     document.getElementById('result').innerHTML = `
     <h2>Success!</h2>
-    <p id>${result}</p>
+    <p>${decodedText}</p>
     `;
-    window.location.href = window.location.href + `?result=${JSON.stringify(result)}`
+    window.location.href = window.location.href + `?result=${JSON.stringify(decodedText)}`
     scanner.clear();
     document.getElementById('reader').remove();
     }
