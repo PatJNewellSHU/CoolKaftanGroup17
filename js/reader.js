0ecb08a318ce1c7a5e38b814aea34ea6e8a91bd1
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2023-04-10 08:24:05 -0700

    commit me

--- js/reader.js
+++ js/reader.js
@@ -8,14 +8,13 @@
     });
     scanner.render(success, error);
     function success(decodedText, decodedResult) {
-
-    document.getElementById('result').innerHTML = `
-    <h2>Success!</h2>
-    <p id="scanned">${decodedText}</p>
-    `;
-    scanner.clear();
-    document.getElementById('reader').remove();
+      window.location.href = window.location.href + "?" + decodedResult + "&" + decodedText;
     }
+
+    //;
+    //scanner.clear();
+    //document.getElementById('reader').remove();
+    //}
     function error(err) {
     console.error(err);
     }
