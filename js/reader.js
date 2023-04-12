b066d8743d66ef211c97f4d6e3eca9285857e931
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2023-04-12 12:41:47 -0700

    added reader comments

--- js/reader.js
+++ js/reader.js
@@ -1,5 +1,3 @@
-let scanResult = ""
-
 
  const scanner = new Html5QrcodeScanner('reader', {
     qrbox: {
@@ -7,18 +5,17 @@ let scanResult = ""
     height: 250,
     },
     fps: 20,
-    });
-    scanner.render(success, error);
+    });//creates reader and its size and how many times it checks for barcode
+    scanner.render(success, error);//upon searching for barcodes if one found calls passed success funciton if no terror function
     function success(result) {
-      scanResult = result.toString()
+      //success function has some kind of result object passe din we cant quite figure out 
       document.getElementById('result').innerHTML = `      
       <h2>Success!</h2>
       <p id="scanned">${result}</p>
-      ;`
-      window.location.href = window.location.href + `?result=${scanResult}`
+      ;`//replaces html of reader with the success and the barcode number that wokrs here for some reason
       scanner.clear();
-      document.getElementById('reader').remove();
+      document.getElementById('reader').remove();//clear and remove scanner part of html so just success html
       }
     function error(err) {
-    console.error(err);
+    console.error(err);//error function if barcode not deteceted
     }
