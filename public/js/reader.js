ba4a6f89379c787c1c2fc977915c750738416a4a
Ethan <bf4spartan@gmail.com>
2023-04-17 17:33:34 +0100

    Little fixes

--- public/js/reader.js
+++ public/js/reader.js
@@ -11,8 +11,7 @@ const scanner = new Html5QrcodeScanner('reader', {
      //success function has some kind of result object passe din we cant quite figure out 
      document.getElementById('result').innerHTML = `      
      <h2>Success!</h2>
-     <p id="scanned">Retrieving details...</p>
-     ;`//replaces html of reader with the success and the barcode number that wokrs here for some reason
+     <p id="scanned">Retrieving details...</p>`//replaces html of reader with the success and the barcode number that wokrs here for some reason
      location.href = location.href + '?scan=' + result;    
      scanner.clear();
      document.getElementById('reader').remove();//clear and remove scanner part of html so just success html
